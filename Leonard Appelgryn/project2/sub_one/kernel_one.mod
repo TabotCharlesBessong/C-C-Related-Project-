ae706e807601d22a379a440fe04db54e88a5d67d
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-03-09 10:17:00 +0100

    removing garbage files

--- Leonard Appelgryn/project2/sub_one/kernel_one.mod
+++
@@ -1,2 +0,0 @@
-/home/yimnai/FET/L-300/firstSemester/CEF-347-operatingSystems/project/sub_one/kernel_one.o
-
--- Yimnai Nerus Zaumu/sub_two/.kernel_two.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/yimnai/FET/L-300/firstSemester/CEF-347-operatingSystems/project/sub_two/kernel_two.mod := { echo  /home/yimnai/FET/L-300/firstSemester/CEF-347-operatingSystems/project/sub_two/kernel_two.o;  echo; } > /home/yimnai/FET/L-300/firstSemester/CEF-347-operatingSystems/project/sub_two/kernel_two.mod
